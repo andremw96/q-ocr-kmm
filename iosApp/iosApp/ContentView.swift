42091f8bcbe6ea2393904fdd05719339a4da0f84
andremw96 <andre19961@gmail.com>
2023-05-05 15:38:31 +0700

    able to take photo from ios

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,17 +1,118 @@
 import SwiftUI
 import shared
 
+extension UIViewController {
+    var topViewController: UIViewController? {
+        if let presented = presentedViewController {
+            return presented.topViewController
+        }
+        if let nav = self as? UINavigationController {
+            return nav.visibleViewController?.topViewController
+        }
+        if let tab = self as? UITabBarController {
+            return tab.selectedViewController?.topViewController
+        }
+        return self
+    }
+}
+
+
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
-        MainScreenIosKt.MainViewController(shouldShowCamera: true as! RuntimeMutableState)
+        MainScreenIosKt.MainViewController(openCameraClicked: { openCameraClicked in
+            if openCameraClicked as! Bool {
+                if let topViewController = UIApplication.shared.windows.first?.rootViewController?.topViewController {
+                    topViewController.present(
+                        UIHostingController(
+                            rootView: MyPickerView(onButtonDismissClicked: {
+                                topViewController.dismiss(animated: true)
+                            })
+                        ),
+                        animated: true,
+                        completion: nil
+                    )
+                }
+            }
+        }, bitmapImageResult: )
     }
 
-    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+}
+
+struct MyPickerView: View {
+    @State private var image: Image? = Image("")
+    @State private var shouldPresentImagePicker = false
+    @State private var shouldPresentActionScheet = false
+    @State private var shouldPresentCamera = false
+    let onButtonDismissClicked: () -> Void
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            MyImagePickerView(
+                sourceType: .photoLibrary,
+                image: self.$image,
+                isPresented: self.$shouldPresentImagePicker
+            )
+            
+//            image!
+//                .resizable()
+//                .aspectRatio(contentMode: .fill)
+//                .frame(width: 300, height: 300)
+//                .clipShape(Circle())
+//                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+//                .shadow(radius: 10)
+//                .onTapGesture { self.shouldPresentActionScheet = true }
+//                .sheet(isPresented: $shouldPresentImagePicker) {
+//                    MyImagePickerView(
+//                        sourceType: self.shouldPresentCamera ? .camera : .photoLibrary,
+//                        image: self.$image,
+//                        isPresented: self.$shouldPresentImagePicker
+//                    )
+//                }
+//                .actionSheet(isPresented: $shouldPresentActionScheet) { () -> ActionSheet in
+//                    ActionSheet(
+//                        title: Text("Choose mode"),
+//                        message: Text("Please choose your preferred mode to set your profile image"),
+//                        buttons:
+//                            [
+////                                ActionSheet.Button.default(
+////                                    Text("Camera"),
+////                                    action: {
+////                                        self.shouldPresentImagePicker = true
+////                                        self.shouldPresentCamera = true
+////
+////                                    }
+////                                ),
+//                                ActionSheet.Button.default(
+//                                    Text("Photo Library"),
+//                                    action: {
+//                                        self.shouldPresentImagePicker = true
+//                                        self.shouldPresentCamera = false
+//
+//                                    }
+//                                 ),
+//                                ActionSheet.Button.cancel()
+//                            ]
+//                    )
+//                }
+//
+            
+            Spacer()
+            
+            Button("Dismiss", action: {
+                onButtonDismissClicked()
+            })
+        }
+    }
 }
 
 struct ContentView: View {
     var body: some View {
         ComposeView()
-                .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
+            .ignoresSafeArea(.keyboard) // Compose has own keyboard handler
     }
 }
--- shared/src/iosMain/kotlin/com/andremw96/qocrkmm/MainScreenIos.kt
+++ shared/src/iosMain/kotlin/com/andremw96/qocrkmm/MainScreenIos.kt
@@ -1,6 +1,13 @@
 package com.andremw96.qocrkmm
 
+import androidx.compose.ui.graphics.toComposeImageBitmap
 import androidx.compose.ui.window.ComposeUIViewController
 import com.andremw96.qocrkmm.ui.MainScreen
+import org.jetbrains.skia.Image
 
-fun MainViewController() = ComposeUIViewController { MainScreen() }
+fun MainViewController(
+    openCameraClicked: (Boolean) -> Unit,
+    bitmapImageResult: Image?,
+) = ComposeUIViewController {
+    MainScreen(openCameraClicked, bitmapImageResult?.toComposeImageBitmap())
+}
